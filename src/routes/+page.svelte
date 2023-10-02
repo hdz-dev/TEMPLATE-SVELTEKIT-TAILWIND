1c71592f3c58334b85d6a7d507ae006bafb31c53
hdz-dev <abigailx1@gmail.com>
2023-10-01 20:20:38 -0500

    PRIMER COMMIT

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-3xl text-center text-white">
+    Hello world!
+  </h1>
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.blue.900);
+    }
+  </style>
